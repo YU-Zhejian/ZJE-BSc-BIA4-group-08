a3316a515acbfaafeac8c23c4849383cb9ea8ad2
yuzj <theafamily@126.com>
2022-11-07 14:13:13 +0800

    sklearn abstraction done

--- src/BIA_G8/model/__init__.py
+++ src/BIA_G8/model/__init__.py
@@ -2,7 +2,15 @@ from __future__ import annotations
 
 from abc import abstractmethod
 
+from typing import Any, Mapping, Type, TypeVar
+
+import joblib
 import numpy.typing as npt
+import tomli
+import tomli_w
+from sklearn.neighbors import KNeighborsClassifier
+
+from BIA_G8.helper import io_helper
 
 
 class AbstractClassifier:
@@ -29,6 +37,37 @@ class AbstractClassifier:
     def predict(self, data: npt.NDArray) -> npt.NDArray:
         pass
 
+    @classmethod
     @abstractmethod
-    def __init__(self, **hyper_params) -> None:
+    def new(self, **hyper_params) -> None:
         pass
+
+
+_SKLearnModelType = TypeVar("_SKLearnModelType")
+
+
+class BaseSklearnClassifier(AbstractClassifier):
+    _model: _SKLearnModelType
+    _model_type: Type[_SKLearnModelType]
+
+    @classmethod
+    def new(cls, **hyper_params) -> BaseSklearnClassifier:
+        new_instance = cls()
+        new_instance._model = new_instance._model_type(**hyper_params)
+        return new_instance
+
+    def fit(self, data: npt.NDArray, label: npt.NDArray) -> BaseSklearnClassifier:
+        self._model.fit(X=data, y=label)
+        return self
+
+    def predict(self, data: npt.NDArray) -> npt.NDArray:
+        return self._model.predict(data)
+
+    @classmethod
+    def load(cls, model_abspath: str) -> BaseSklearnClassifier:
+        new_instance = cls()
+        new_instance._model = joblib.load(model_abspath)
+        return new_instance
+
+    def save(self, model_abspath: str) -> None:
+        joblib.dump(self._model, model_abspath)
--- src/BIA_G8/model/sklearn_knn.py
+++ src/BIA_G8/model/sklearn_knn.py
@@ -1,46 +1,31 @@
 from __future__ import annotations
 
-from typing import Any, Mapping
-
-import numpy.typing as npt
-import tomli
-import tomli_w
+import numpy as np
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 
-from BIA_G8.helper import io_helper
-from BIA_G8.model import AbstractClassifier
+from BIA_G8.model import BaseSklearnClassifier
 
 
-class SklearnKNearestNeighborsClassifier(AbstractClassifier):
+class SklearnKNearestNeighborsClassifier(BaseSklearnClassifier):
     _name = "SklearnKNearestNeighborsClassifier"
-    _knn: KNeighborsClassifier
-    _hyper_params: Mapping[str, Any]
+    _model_type= KNeighborsClassifier
 
-    def __init__(self, **hyper_params) -> None:
-        self._hyper_params = hyper_params
-        self._knn = KNeighborsClassifier(**hyper_params)
 
-    def fit(self, data: npt.NDArray, label: npt.NDArray) -> SklearnKNearestNeighborsClassifier:
-        self._knn.fit(X=data, y=label)
-        return self
+if __name__ == '__main__':
+    x, y = make_classification(
+        n_samples=12000,
+        n_features=7,
+        n_classes=3,
+        n_clusters_per_class=1
+    )
+    x_train, x_test, y_train, y_test = train_test_split(x, y)
 
-    def predict(self, data: npt.NDArray) -> npt.NDArray:
-        return self._knn.predict(data)
+    m = SklearnKNearestNeighborsClassifier.new()
+    m.fit(x_train, y_train)
+    m.save("tmp.pkl.xz")
+    del m
+    m2 = SklearnKNearestNeighborsClassifier.load("tmp.pkl.xz")
+    print(np.sum(m2.predict(x_test) == y_test)*100/len(y_test))
 
-    @classmethod
-    def load(cls, model_abspath: str) -> SklearnKNearestNeighborsClassifier:
-        hyper_params_path = model_abspath + ".hyper_params.toml"
-        params = io_helper.read_pickle_xz(model_abspath)
-        with open(hyper_params_path, "rb") as reader:
-            hyper_params = tomli.load(reader)
-        new_instance = cls(**hyper_params)
-        new_instance._knn.set_params(**params)
-        return new_instance
-
-    def save(self, model_abspath: str) -> None:
-        hyper_params_path = model_abspath + ".hyper_params.toml"
-        params = self._knn.get_params()
-        hyper_params = self._hyper_params
-        io_helper.write_pickle_xz(params, model_abspath)
-        with open(hyper_params_path, "wb") as writer:
-            tomli_w.dump(hyper_params, writer)
