4147946548d4d1fd383c6bb683e36b19fe48be13
yuzj <theafamily@126.com>
2022-12-07 18:59:19 +0800

    UI support added

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 """``setup.py`` that is replaced by ``pyproject.toml``"""
-
 from setuptools import setup
 
+from compile_ui import compile_all_ui
+
 if __name__ == '__main__':
+    compile_all_ui()
     setup()
